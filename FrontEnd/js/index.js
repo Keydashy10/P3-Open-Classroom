a5c11ff59795812d49f99bbc0e3e0946ea0bf96e
Keydashy TES <keydashy.tes@gmail.com>
2023-12-23 19:41:35 +0100

    Ouverture de la modale qui est peuplé avec l'API.

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -13,13 +13,20 @@ function init(){
         <i class="fa-regular fa-pen-to-square"></i> modifier
          </a>
         `)
+        const modif = document.querySelector('.modif')
+        const modale = document.getElementById('modale')
+        modif.addEventListener('click', (e)=>{
+                e.preventDefault()
+                // On fait un Appel API pour peupler la modale (#modale)
+                getWorks(document.getElementById('contenu-modale'), false)
+                // Avec le résultat de l'appel API tu ajoutes les travaux à la modale
+                // On affiche la modale
+                modale.showModal()
+        })
+        
 
-        // const modifya = document.createElement('a')
-        // modifya.innerHTML = `<div class="edition-mode">
-        // <i class="fa-regular fa-pen-to-square"></i> modifier
-        // </div>` 
-        // document.body.getElementById('portfolio').appendChild(modifya)
-
+        // Faire en sorte que si la modale est ouverte, sur l'appuie sur la touche "esc", la modale se ferme
+        
         document.querySelector('header').classList.add('header-connect')
         const tous= document.querySelector('.filters button')
         // On supprime le bouton "tous"
@@ -37,7 +44,7 @@ function init(){
     if(!token){
        getCategories() 
     }
-    getWorks()
+    getWorks(document.querySelector('.gallery'))
 }
 init()
 
@@ -86,21 +93,36 @@ async function getCategories() {
 
 
 // Fonction qui permet de récupérer les travaux et les afficher
-async function getWorks(){
+async function getWorks(element, showCaption = true){
     // Faire appel au backend, sur la bonne URL
     const endpoint = "http://localhost:5678/api/works"
 
     const req = await fetch(endpoint)
     const datas = await req.json()
     
-    const gallery = document.querySelector('.gallery')
+    // const gallery = document.querySelector('.gallery')
     // On parcours les données une a une pour les afficher dans la page
-    datas.forEach(data => {
+    showFigure(element, datas, showCaption)
+
+}
+
+function showFigure(element, datas, showCaption = true){
+
+    element.innerHTML = "";
+
+    datas.forEach(data =>{
+        var caption = ``;
+
+        if (showCaption) 
+            caption = `<figcaption>${data.title}</figcaption>`;
+
         const figure = `<figure data-category="${data.categoryId}">
-                            <img src="${data.imageUrl}" alt="${data.title}">
-                            <figcaption>${data.title}</figcaption>
-                        </figure>`
-        gallery.insertAdjacentHTML("beforeend", figure)
+        <img src="${data.imageUrl}" alt="${data.title}" `+ caption +`
+        </figure>`   
+
+        element.insertAdjacentHTML("beforeend", figure)
+
     })
 
+ 
 }
